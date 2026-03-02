b8fbdaec0c4c0892f8bb9ab00e8ca46881082a4e
av25754 <voicuandrei378@gmail.com>
2026-03-02 12:45:37 -0500

    Create Customer.js

---
+++ Customer.js
@@ -0,0 +1,21 @@
+class Customer {
+  constructor(id, location, passengers = 1, amenitiesRequired = []) {
+    this.id = id;
+    this.location = location;
+    this.passengers = passengers;
+    this.amenitiesRequired = amenitiesRequired;
+
+    this.requestTime = millis();
+  }
+
+  display() {
+    fill(100,100,255); //blue
+    rectMode(CENTER);
+    rect(this.location.x, this.location.y, 18, 18);
+
+    fill(255);
+    textSize(10);
+    textAlign(CENTER);
+    text(this.id, this.location.x, this.location.y - 14);
+  }
+}
