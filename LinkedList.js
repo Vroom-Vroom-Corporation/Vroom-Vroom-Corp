f7a1d605a3b848c287a7912152ab752aa872bdd7
av25754 <voicuandrei378@gmail.com>
2026-03-02 12:45:19 -0500

    Create LinkedList.js

---
+++ LinkedList.js
@@ -0,0 +1,21 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(data) {
+    // STUDENTS IMPLEMENT
+  }
+
+  delete(data) {
+    // STUDENTS IMPLEMENT
+  }
+
+  search(predicate) {
+    // STUDENTS IMPLEMENT
+  }
+
+  traverse(callback) {
+    // STUDENTS IMPLEMENT
+  }
+}
